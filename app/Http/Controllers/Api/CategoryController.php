2bd2111df2d8e0d805b62934dc319694ba40e553
Levan <e00xff@gmail.com>
2022-07-03 20:16:11 +0400

    Eloquent API Resource and Data Wrapping

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -15,12 +16,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return Category::all();
-    }
-
-    public function index_v2()
-    {
-        return Category::select('id', 'name')->get();
+        return CategoryResource::collection(Category::select('id', 'name')->get());
     }
 
     /**
