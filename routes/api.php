ba7e2008e324c05aba18cbbba7a40ec2d4e3d5db
Levan <e00xff@gmail.com>
2022-07-03 20:43:56 +0400

    Show() with RouteModelBinding and Route::apiResource

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('categories', [CategoryController::class, 'index']);
+//Route::get('categories', [CategoryController::class, 'index']);
+//Route::get('categories/{category}', [CategoryController::class, 'show']);
+
+Route::resource('categories', CategoryController::class);
