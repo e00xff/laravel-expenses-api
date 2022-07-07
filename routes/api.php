1fe25fc06f1febd0ce2fd19629d664c68c849e2e
Levan <e00xff@gmail.com>
2022-07-07 15:02:05 +0400

    General Sanctum Auth: Get/Use Tokens

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\Api\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,12 +20,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('categories', [CategoryController::class, 'index']);
+//Route::get('categories/{category}', [CategoryController::class, 'show']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::get('categories', [CategoryController::class, 'index']);
-//Route::get('categories/{category}', [CategoryController::class, 'show']);
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('transactions', TransactionController::class);
+});
 
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('transactions', TransactionController::class);
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
